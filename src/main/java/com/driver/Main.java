20f6a52194bb876060269a1326c4d861e9e407b3
mayurW1000 <mayur.wable1000@gmail.com>
2022-10-08 18:42:39 +0530

    code done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+    RWOnly obj = new RWOnly();
+    //obj.data = 15;
+    obj.setName("shubham");
+    System.out.println(obj.getName());
+}
 }
